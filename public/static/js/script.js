be0a6d5774f8fa4459820fa42c73e3b44fb267d3
Peter <peter.duke.10@gmail.com>
2019-12-20 13:08:41 -0500

    initial commit

---
+++ public/static/js/script.js
@@ -0,0 +1,20 @@
+
+$( window ).ready(function(){
+    var video = document.getElementsByTagName('video')[0];
+    video.addEventListener('ended', function() {
+    video.load();
+    $(".main_video").addClass("finished");
+    video.autoplay=false;
+}, false);
+});
+var video = document.getElementById('video');
+var video_btn = document.getElementById('video-btn');
+var btn_status = 0;
+
+
+video_btn.addEventListener('click', function () {
+    video.play();
+    video.currentTime = 0 ;
+    $(".main_video").removeClass("finished");
+    btn_status = 1;
+});
